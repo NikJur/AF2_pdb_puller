12d950e4c1e6ed6e116cfebd4fc315f9845fb094
NikJur <93187448+NikJur@users.noreply.github.com>
2023-03-10 13:52:51 +0000

    downloadable url finder
    extracts download urls from website

--- 1_basic_puller.py
+++ 1_basic_puller.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 # URL of the website containing the PDB files
 url = 'https://alphafold.ebi.ac.uk/entry/'
@@ -10,6 +11,20 @@ pdb_files = ['Q5VSL9', 'A0A6M0PRZ6']
 for file in pdb_files:
     # Construct the URL for the current PDB file
     pdb_url = url + file
+    
+        ###FIND DOWNLOADABLE LINKS####
+        # Send a request to the server to get the webpage content
+        response = requests.get(url)
+    
+        # Parse the webpage content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Find all <a> tags that contain the download links
+        download_links = soup.find_all('a', href=True)
+    
+        # Loop through the download links and print their URLs
+        for link in download_links:
+            print(link['href'])
     
     # Send a request to the server to download the PDB file
     response = requests.get(pdb_url)
